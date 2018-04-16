c2f3b08a6e31341591dfaf705e4f4f177ee88ff7
Renan Roberto <renanroberto1@gmail.com>
2018-04-15 21:50:52 -0300

    mgo working

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 	"runtime"
 
 	"api/router"
+
+	"gopkg.in/mgo.v2"
 )
+
+type Doc struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
 func main() {
 	router := router.NewRouter()
@@ -19,6 +26,30 @@ func main() {
 	fmt.Printf("| GOMAXPROCS: %d        |\n", runtime.GOMAXPROCS(0))
 	fmt.Printf("| Goroutines: %d        |\n", runtime.NumGoroutine())
 	fmt.Printf(" ---------------------- \n\n")
+
+	// -------------------------------- MGO TEST --------------------------------
+
+	mongoURI := "mongodb://renan:renan@ds259865.mlab.com:59865/db-test"
+
+	mongoInfo, err := mgo.ParseURL(mongoURI)
+	if err != nil {
+		panic(err)
+	}
+
+	session, err := mgo.DialWithInfo(mongoInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	collection := session.DB("db-test").C("users")
+
+	doc := Doc{"Renan", "renanroberto1@gmail.com"}
+	err = collection.Insert(doc)
+	if err != nil {
+		panic(err)
+	}
+
+	// -------------------------------- MGO TEST --------------------------------
 
 	fmt.Println("Server is running on http://localhost:8080/")
 	log.Fatal(http.ListenAndServe(":8080", router))
